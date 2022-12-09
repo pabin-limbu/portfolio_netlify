f9f96e9c7b407a6cbc55216199ad19c6de33c394
pabin-limbu <changsu.pabin@gmail.com>
2022-12-09 17:11:27 +0800

    react router set to app.js file in order to create route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          New portfolio project to be deployed in netlify this is the new
-          changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
     </div>
   );
 }
