9be370de54025222a0e7645b16c173a338756b32
pabin-limbu <changsu.pabin@gmail.com>
2022-12-09 17:14:04 +0800

    sidebar component  added to layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { Outlet } from "react-router-dom";
+import Sidebar from "../Sidebar";
+import "./index.scss";
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <div>
+      <Sidebar />
+      <div className="page">
+        {/* The use of outlet in parent element allow us to render the nested route element. */}
+        <Outlet />
+        <div className="footer">
+          <p>Pabin Limbu &copy; 2022 </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Layout;
